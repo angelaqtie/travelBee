9597c6cae67efb2ee48c83a722a61684b9aea829
Lornz18 <mellgodwinbarza22@gmail.com>
2024-10-10 15:21:43 +0800

    fg

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+var slider = tns({
+    container: ".footer__images1--slider",
+    items: 6,
+    speed: 200,
+    slideBy: 1,
+    autoplay: false,
+    containerControls: "#controls",
+    prevButton: ".prev",
+    nextButton: ".next",
+
+});
